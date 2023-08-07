1b33e6cd33c137e4c50be1580712dd8f596a47e3
Pratik Patil <cryptowriter.eth@gmail.com>
2023-08-07 15:40:25 +0530

    added different bg depending on weather condition

--- src/app/components/Dashboard/Dashboard.js
+++ src/app/components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import WeatherDataContext from "@/app/context/WeatherDataContext";
 import { useContext, useEffect, useState } from "react";
+import "./Dashboard.css";
 
 export default function Dashboard() {
   const { currentWeatherData } = useContext(WeatherDataContext);
@@ -7,6 +8,7 @@ export default function Dashboard() {
   const [day, setDay] = useState("");
   const [weatherCond, setWeatherCond] = useState("");
   const [weatherCondition, setWeatherCondition] = useState("");
+  const [videoLink, setVideoLink] = useState("");
 
   // useEffect(() => {
   //   console.log(currentWeatherData);
@@ -57,40 +59,77 @@ export default function Dashboard() {
     setClock(time);
   }, []);
 
+  // Different Weather backgrounds as per weather conditions
+
+  useEffect(() => {
+    switch (weatherCondition) {
+      case "Clear":
+        setVideoLink("https://i.imgur.com/CMPyHeG.mp4");
+        break;
+      case "Rain":
+        setVideoLink("https://i.imgur.com/XCqhve1.mp4");
+        break;
+      case "Clouds":
+        setVideoLink("https://i.imgur.com/MPGmERW.mp4");
+        break;
+      case "Snow":
+        setVideoLink("https://i.imgur.com/jJJkFn4.mp4");
+        break;
+      case "Drizzle":
+        setVideoLink("https://i.imgur.com/Tfz3YxS.mp4");
+        break;
+      case "Thunderstorm":
+        setVideoLink("https://i.imgur.com/dIR6jS1.mp4");
+        break;
+      default:
+        setVideoLink("https://i.imgur.com/d7kZray.mp4");
+    }
+  }, [weatherCondition]);
+
   return (
-    <div className="w-full md:w-2/4 h-60 bg-gray-200 mr-4 rounded-xl overflow-hidden flex flex-col justify-between my-4">
-      <div className="flex justify-between py-2 px-2 ">
-        <div>
-          <h1>{day}</h1>
-        </div>
-        <div>
-          <h1>{clock}</h1>
-        </div>
-      </div>
-      <div className="flex flex-col m-auto items-center w-40">
-        <div className="flex w-full justify-evenly">
+    <div className="w-full md:w-2/4 h-60 bg-gray-200 mr-4 rounded-xl overflow-hidden flex flex-col justify-between my-4 relative">
+      <div className="overlay absolute w-full h-full top-0 right-0 rounded-xl"></div>
+      <video
+        src={videoLink}
+        className="w-full h-full object-cover	rounded-xl"
+        autoPlay
+        loop
+        muted
+      />
+      <div className="absolute w-full h-full top-0 right-0 text-white">
+        <div className="flex justify-between py-2 px-2 ">
           <div>
-            <h1 className="text-7xl font-semibold mr-1">
-              {Math.floor(currentWeatherData?.main?.temp)}&deg;
+            <h1>{day}</h1>
+          </div>
+          <div>
+            <h1>{clock}</h1>
+          </div>
+        </div>
+        <div className="flex flex-col m-auto items-center w-40 py-8">
+          <div className="flex w-full justify-evenly ">
+            <div>
+              <h1 className="text-7xl font-semibold mr-1">
+                {Math.floor(currentWeatherData?.main?.temp)}&deg;
+              </h1>
+            </div>
+            <div className="border-l-2 border-white pl-4 my-auto">
+              <h1>{Math.floor(currentWeatherData?.main?.temp_min)}&deg;</h1>
+              <h1>{Math.floor(currentWeatherData?.main?.temp_max)}&deg;</h1>
+            </div>
+          </div>
+          <div>
+            <h1>{weatherCond}</h1>
+          </div>
+        </div>
+        <div className="flex justify-between py-2 px-2">
+          <div>
+            <h1>
+              Real Feel: {Math.floor(currentWeatherData?.main?.feels_like)}&deg;
             </h1>
           </div>
-          <div className="border-l-2 border-black pl-4 my-auto">
-            <h1>{Math.floor(currentWeatherData?.main?.temp_min)}&deg;</h1>
-            <h1>{Math.floor(currentWeatherData?.main?.temp_max)}&deg;</h1>
+          <div>
+            <h1>{weatherCondition}</h1>
           </div>
-        </div>
-        <div>
-          <h1>{weatherCond}</h1>
-        </div>
-      </div>
-      <div className="flex justify-between py-2 px-2">
-        <div>
-          <h1>
-            Real Feel: {Math.floor(currentWeatherData?.main?.feels_like)}&deg;
-          </h1>
-        </div>
-        <div>
-          <h1>{weatherCondition}</h1>
         </div>
       </div>
     </div>
